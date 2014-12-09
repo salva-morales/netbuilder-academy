00cb91dbd17f4af376699703c76bc325ad15699a
salva <salva.morales@netbuilder.com>
2014-12-09 19:28:47 +0000

    fixing java compiler

--- academy/core/src/main/java/academy/core/taglibs/ListEmptyRoomsTag.java
+++ academy/core/src/main/java/academy/core/taglibs/ListEmptyRoomsTag.java
@@ -3,4 +3,9 @@ package academy.core.taglibs;
 import javax.servlet.jsp.tagext.TagSupport;
 
 public class ListEmptyRoomsTag extends TagSupport{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 }
